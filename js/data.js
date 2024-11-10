40ba322bb19bb431c61af1cc57750d06633654c1
MariaEduarda15max <eduardadarosa7412@gmail.com>
2024-11-09 22:14:29 -0300

    tarea realizada

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const storedData = localStorage.getItem("storedData");
-    document.getElementById("data").textContent = storedData ? storedData : "No hay datos guardados";
+document.addEventListener("DOMContentLoaded", function(){
+    const storedData = localStorage.getItem("storedData"); // obtiene el valor almacenado en el localstorage desde el input text y el botón guardar
+    document.getElementById("data").textContent  = storedData || "No hay datos almacenados.";
 });
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById("buttonText").addEventListener("click", function() {
+document.getElementById("buttonText").addEventListener("click", function(){
     const inputText = document.getElementById("inputText").value;
-    localStorage.setItem("storedData", inputText);
+    localStorage.setItem("storedData", inputText); //guardar valor en el localstorage
 });
